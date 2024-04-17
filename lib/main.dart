e57bc57d267ab3ec77d01cddfa4dd86f0f56d519
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2024-04-17 14:11:41 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpenseApp());
+}
+
+class ExpenseApp extends StatelessWidget {
+  const ExpenseApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Expense Tracker App",
+      home: ,
+    )
+  }
+}
